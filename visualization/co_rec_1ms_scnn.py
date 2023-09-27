5fea897d4b81ceff7829c347a46ae2371c2e3a7a
Juan Pablo Romero Bermudez <jprb@kth.se>
2023-09-27 20:56:58 +0200

    Video Saver (semi-efficient)

--- visualization/co_rec_1ms_scnn.py
+++ visualization/co_rec_1ms_scnn.py
@@ -7,6 +7,7 @@ import pdb
 import aestream
 import cv2
 import sys
+import os
 sys.path.append('../common')
 from tools import Dimensions, get_shapes
 import argparse
@@ -14,6 +15,8 @@ import matplotlib.pyplot as plt
 import socket
 import random
 import time
+
+from datetime import datetime
 
 # IP and port of the receiver (Computer B)
 receiver_ip = "172.16.222.30"
@@ -28,6 +31,8 @@ def parse_args():
     parser.add_argument('-p1', '--port-1', type= int, help="Port for events", default=3331)
     parser.add_argument('-vs', '--vis-scale', type=int, help="Visualization scale", default=1)
     parser.add_argument('-m', '--mode', type= str, help="Mode", default="both")
+    parser.add_argument('-at', '--acc-time', type= int, help="Accumulation Time", default=1)
+    parser.add_argument('-d', '--duration', type= int, help="Duration", default=1)
 
     return parser.parse_args()
 
@@ -40,53 +45,62 @@ if __name__ == '__main__':
     dim = Dimensions.load_from_file('../common/homdim.pkl')
     
 
-    max_nb_frames = 1000
+    max_nb_frames = int(args.duration*1000/args.acc_time)
+    print(f"Max # frames: {max_nb_frames}")
+    # pdb.set_trace()
 
-    original_img = torch.zeros(1, 1, dim.fl, dim.fw)
-    convolved_img = torch.zeros(1, 1, dim.fl, dim.fw)
+    original_img = torch.zeros(1, 1, 640, 480)
+    convolved_img = torch.zeros(1, 1, 640, 480)
 
-    orig_out = np.zeros((max_nb_frames,dim.fl, dim.fw))
-    conv_out = np.zeros((max_nb_frames,dim.fl, dim.fw))
+    orig_out = np.zeros((max_nb_frames,640, 480))
+    conv_out = np.zeros((max_nb_frames,640, 480))
 
 
     frame_counter = 0
     read_counter = 0
-    elapsed_time = np.zeros((max_nb_frames*10,1))
-    with aestream.UDPInput((dim.fl, dim.fw), device = 'cpu', port=args.port_0) as stream_0:
-        with aestream.UDPInput((dim.fl, dim.fw), device = 'cpu', port=args.port_1) as stream_1:
+    beginning = time.perf_counter()
+    elapsed_time = 0
+    with aestream.UDPInput((640, 480), device = 'cpu', port=args.port_0) as stream_0:
+        with aestream.UDPInput((640, 480), device = 'cpu', port=args.port_1) as stream_1:
             
             start_time = time.perf_counter()
             last_time = time.perf_counter()
             while True:
 
-                # pdb.set_trace()
-                original_img[0,0,:,:] += stream_0.read()
-                convolved_img[0,0,:,:] += stream_1.read()
-                
-                now = time.perf_counter()
-                elapsed_time[read_counter] = (now - start_time)
-
-                if elapsed_time[read_counter] > 1e-3:
-                    orig_out[frame_counter,:,:] = (original_img * 255.0).clamp(0, 255).to(torch.uint8).squeeze().numpy()
-                    conv_out[frame_counter,:,:] = (convolved_img * 255.0).clamp(0, 255).to(torch.uint8).squeeze().numpy()
-                    original_img = torch.zeros(1, 1, dim.fl, dim.fw)
-                    convolved_img = torch.zeros(1, 1, dim.fl, dim.fw)
-                    frame_counter += 1
-                    start_time = now
+                try:
+                    # pdb.set_trace()
+                    original_img[0,0,:,:] += stream_0.read()
+                    convolved_img[0,0,:,:] += stream_1.read()
                     
-                read_counter +=1 
-                last_time = now
+                    now = time.perf_counter()
+                    elapsed_time += now - start_time
+
+                    if elapsed_time > args.acc_time/1000:
+                        # print(elapsed_time[-1])
+                        orig_out[frame_counter,:,:] = (original_img * 255.0).clamp(0, 255).to(torch.uint8).squeeze().numpy()
+                        conv_out[frame_counter,:,:] = (convolved_img * 255.0).clamp(0, 255).to(torch.uint8).squeeze().numpy()
+                        original_img = torch.zeros(1, 1, 640, 480)
+                        convolved_img = torch.zeros(1, 1, 640, 480)
+                        frame_counter += 1
+                        start_time = now
+                        
+                    read_counter +=1 
+                    last_time = now
+                except:
+                    pdb.set_trace()
 
                 if frame_counter >= max_nb_frames:
+                    print("C'est fini")
                     break
 
+    whole_duration = time.perf_counter() - beginning
+    print(f"Whole recording lasted: {whole_duration:.3f} seconds")
 
     print(f"{np.sum(orig_out)} events")
     print(f"{np.sum(conv_out)} events")
-    mean = 1e6*np.mean(elapsed_time)
-    std = 1e6*np.std(elapsed_time)
+    mean = 1e6*elapsed_time/read_counter
+    print(f"Average reading time: {mean:.6f} [us]")
 
-    print(f"Average reading time: {mean:.6f} [us] +- {std}")
 
 
     if args.mode == "both":
@@ -104,35 +118,58 @@ if __name__ == '__main__':
     kernel = np.load("../common/kernel.npy")
     k_sz = len(kernel)
     # pdb.set_trace()
-    black = np.zeros((dim.fl,dim.fw,3), dtype=np.uint8)
-    frame = black
+    black = np.zeros((640,480,3), dtype=np.uint8)
+    
+    old_frame = black
+    new_frame = black
+
+
+
+    # Get the current date and time
+    current_datetime = datetime.now()
+    output_file = f"{current_datetime.year}_{current_datetime.month:02d}_{current_datetime.day:02d}_{current_datetime.hour:02d}_{current_datetime.minute:02d}_{args.acc_time}ms.avi"
+    print(f"File name: {output_file}")
+    # pdb.set_trace()
 
     # Define video properties
-    output_file = f'output_video_{args.mode}.avi'
     frame_rate = 25  # Frames per second (adjust as needed)
     frame_duration_ms = 40  # Duration of each frame in milliseconds
 
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(output_file, fourcc, frame_rate, (dim.fl, dim.fw))
+    out = cv2.VideoWriter(output_file, fourcc, frame_rate, (640, 480))
 
     orig_out = orig_out.transpose((1, 2, 0))
     conv_out = conv_out.transpose((1, 2, 0))
 
+    empty_frame_counter = 0
+    for i in range(max_nb_frames):
+        if np.sum(orig_out[:,:,i]) == 0:
+            empty_frame_counter +=1
+
+    print(f"{empty_frame_counter}/{max_nb_frames} frames are empty")
+
     # Iterate through the frames and write to the video
     for i in range(max_nb_frames):
         if flag_orig:
-            frame[:,:,1] = orig_out[:, :, i]
+            new_frame[:,:,1] = orig_out[:, :, i]
         if flag_conv:
-            frame[int(k_sz/2):dim.fl-int(k_sz/2),int(k_sz/2):dim.fw-int(k_sz/2),0] = conv_out[0:dim.fl-k_sz+1,0:dim.fw-k_sz+1, i]
+            new_frame[int(k_sz/2):640-int(k_sz/2),int(k_sz/2):480-int(k_sz/2),0] = conv_out[0:640-k_sz+1,0:480-k_sz+1, i]
                     
+        # pdb.set_trace()
+        if np.sum(new_frame)==0:
+            if i > 0:
+                new_frame = old_frame
+        else:        
+            old_frame = new_frame
 
         # Write the frame to the video
         for _ in range(frame_duration_ms // (1000 // frame_rate)):
-            out.write(frame.transpose(1,0,2))
+            out.write(new_frame.transpose(1,0,2))
 
     # Release the VideoWriter object
     out.release()
+    os.system(f"mv {output_file} videos/")
 
     # Optional: Show a message when the video creation is complete
     print(f"Video saved as {output_file}")
