1a3afd85ea54af1a8dd44744f733e37a36fdf14e
Juan-Pablo Romero Bermudez <jprb@kth.se>
2023-10-03 20:32:58 +0200

    Decreased delay + problem with cnn --> xyp

--- convolution/rc_scnn_tracker.py
+++ convolution/rc_scnn_tracker.py
@@ -13,9 +13,40 @@ import math
 import time
 import sys
 import select
+import csv
 sys.path.append('../common')
 from tools import Dimensions
 
+
+def save_tuples_to_csv(data, filename):
+    """
+    Save a list of tuples to a CSV file.
+
+    Args:
+        data (list of tuples): The data to be saved.
+        filename (str): The name of the CSV file to create or overwrite.
+    """
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        for row in data:
+            writer.writerow(row)
+
+def create_conn_list(width, height):
+    delay = 1 # [ms]
+    weight = 56
+    conn_list = []
+    for idx in range(width*height):
+        pre_idx = idx
+        x = int(idx % width)
+        y = int(idx / width)
+
+        post_idx = x
+        conn_list.append((pre_idx, post_idx, weight, delay))
+        post_idx = width + y
+        conn_list.append((pre_idx, post_idx, weight, delay))
+
+    save_tuples_to_csv(conn_list, "my_conn_list.csv")
+    return conn_list
 
 def input_with_timeout(prompt, timeout):
     print(prompt, end='', flush=True)
@@ -124,26 +155,32 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description='Automatic Coordinate Location')
 
-    parser.add_argument('-vpo', '--vis-port', type= int, help="Port Out", default=3331)
-    parser.add_argument('-vip', '--vis-ip', type= str, help="IP out", default="172.16.222.199")
-    parser.add_argument('-wpo', '--wta_port', type= int, help="Port Out", default=3333)
-    parser.add_argument('-wip', '--wta_ip', type= str, help="IP out", default="172.16.223.10")
+    parser.add_argument('-pc', '--port-cnn', type= int, help="Port Out", default=3331)
+    parser.add_argument('-pp', '--port-xyp', type= int, help="Port Out", default=3334)
+    parser.add_argument('-ip', '--ip', type= str, help="IP out", default="172.16.222.199")
     parser.add_argument('-ks', '--ks', type=int, help="Kernel Size", default=45)
     parser.add_argument('-b', '--board', type=int, help="Board ID", default=1)
     parser.add_argument('-ws', '--w-scaler', type=float, help="Weight Scaler", default=0.80)
     parser.add_argument('-tm', '--tau-m', type=float, help="Tau m", default=3.0)
     parser.add_argument('-th', '--thickness', type=int, help="Kernel edge thickness", default=2)
     parser.add_argument('-rt', '--runtime', type=int, help="Runtime in [s]", default=240)
+    parser.add_argument('-md', '--mode', type=str, help="SPIF Out: cnn or xyp", default="cnn")
     return parser.parse_args()
 
 if __name__ == '__main__':
 
     args = parse_args()
+    if args.mode == "cnn":
+        showing_cnn = True 
+    else:
+        showing_cnn = False 
+    
 
     dim = Dimensions.load_from_file('../common/homdim.pkl')
     print("Setting machines up ... ")
     CFG_FILE = f"spynnaker_{args.board}.cfg"
-    SPIF_IP = spin_spif_map[f"{args.board}"]
+    SPIF_IP_A = "172.16.223.2"
+    SPIF_IP_B = "172.16.223.16"
 
 
     print("Generating Kernel ... ")
@@ -154,6 +191,9 @@ if __name__ == '__main__':
     SUB_HEIGHT = 8
     WIDTH = dim.fl
     HEIGHT = dim.fw
+    OUT_WIDTH = WIDTH-len(kernel)+1
+    OUT_HEIGHT = HEIGHT-len(kernel)+1
+
 
     pow_2 = [1,2,3,4,5]
     nb_cores = 24 * 48 * 16
@@ -169,10 +209,9 @@ if __name__ == '__main__':
     NPC_X = x*2
     NPC_Y = y*2
 
-    VIS_IP = args.vis_ip
-    VIS_PORT = args.vis_port
-    WTA_IP = args.wta_ip
-    WTA_PORT = args.wta_port
+    MY_PC_IP = args.ip
+    MY_PC_PORT_CNN = args.port_cnn
+    MY_PC_PORT_XYP = args.port_xyp
     SPIF_PORT = 3332
     POP_LABEL = "target"
     RUN_TIME = 1000*60*args.runtime
@@ -188,40 +227,34 @@ if __name__ == '__main__':
     global sock 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-
     def recv_nid(label, spikes):
         global sock
-        vis_data = b""
-        wta_data = b""
+        data = b""
         np_spikes = np.array(spikes)
-        # max_x_wta = 0
-        # max_y_wta = 0
-        # min_x_wta = 1000
-        # min_y_wta = 1000
         for i in range(np_spikes.shape[0]):      
-            x_vis = int(np_spikes[i]) % out_width
-            y_vis = int(int(np_spikes[i]) / out_width)
-            x_wta = int(x_vis/2)
-            y_wta = int(y_vis/2)
-            # # update maxs
-            # if x_wta > max_x_wta:
-            #     max_x_wta = x_wta
-            # if y_wta > max_y_wta:
-            #     max_y_wta = y_wta
-            # # update mins
-            # if x_wta < min_x_wta:
-            #     min_x_wta = x_wta
-            # if y_wta < min_y_wta:
-            #     min_y_wta = y_wta
+            x = int(np_spikes[i]) % OUT_WIDTH
+            y = int(int(np_spikes[i]) / OUT_WIDTH)
             polarity = 1
-            packed_vis = (NO_TIMESTAMP + (polarity << P_SHIFT) + (y_vis << Y_SHIFT) + (x_vis << X_SHIFT))
-            packed_wta = (NO_TIMESTAMP + (polarity << P_SHIFT) + (y_wta << Y_SHIFT) + (x_wta << X_SHIFT))
-            vis_data += pack("<I", packed_vis)
-            wta_data += pack("<I", packed_wta)
-        sock.sendto(vis_data, (VIS_IP, VIS_PORT))
-        sock.sendto(wta_data, (WTA_IP, WTA_PORT))
-        # print(f"{max_x_wta},{max_y_wta},{min_x_wta},{min_y_wta}")
+            packed = (NO_TIMESTAMP + (polarity << P_SHIFT) + (y << Y_SHIFT) + (x << X_SHIFT))
+            data += pack("<I", packed)
+        sock.sendto(data, (MY_PC_IP, MY_PC_PORT_CNN))
 
+
+    def recv_nid_rc(label, spikes):
+        global sock
+        data = b""
+        np_spikes = np.array(spikes)
+        for i in range(np_spikes.shape[0]):    
+            x = int(np_spikes[i]) % (OUT_WIDTH+OUT_HEIGHT)
+            y = int(int(np_spikes[i]) / (OUT_WIDTH+OUT_HEIGHT))
+            if(x<OUT_WIDTH):
+                print(f"x:{x}")
+            else:
+                print(f"\t\ty:{x-OUT_WIDTH}")
+            polarity = 1
+            packed = (NO_TIMESTAMP + (polarity << P_SHIFT) + (y << Y_SHIFT) + (x << X_SHIFT))
+            data += pack("<I", packed)
+        sock.sendto(data, (MY_PC_IP, MY_PC_PORT_XYP))
 
     print("Creating Network ... ")
 
@@ -236,40 +269,61 @@ if __name__ == '__main__':
                 'i_offset': 0.0
                 }
 
-    conn = p.external_devices.SPIFLiveSpikesConnection(
-        [POP_LABEL], SPIF_IP, SPIF_PORT)
-    conn.add_receive_callback(POP_LABEL, recv_nid)
 
 
     p.setup(timestep=1.0, n_boards_required=24)
 
+
+    IN_POP_LABEL = "input"
+    CNN_POP_LABEL = "convolution"
+    XYP_POP_LABEL = "xyproj"
+
     celltype = p.IF_curr_exp
     p.set_number_of_neurons_per_core(celltype, (NPC_X, NPC_Y))
 
-    spif_retina = p.Population(
-        WIDTH * HEIGHT, p.external_devices.SPIFRetinaDevice(
-            pipe=0, width=WIDTH, height=HEIGHT,
-            sub_width=SUB_WIDTH, sub_height=SUB_HEIGHT, chip_coords=CHIP),
-        label="retina")
 
+    # Setting up SPIF Input
+    p_spif_virtual = p.Population(WIDTH * HEIGHT, p.external_devices.SPIFRetinaDevice(
+                                  pipe=0, width=WIDTH, height=HEIGHT,
+                                  sub_width=SUB_WIDTH, sub_height=SUB_HEIGHT, 
+                                  chip_coords=CHIP), label=IN_POP_LABEL)
+
+    # Setting up Convolutional Layer
     convolution = p.ConvolutionConnector(kernel_weights=kernel)
-    out_width, out_height = convolution.get_post_shape((WIDTH, HEIGHT))
+    cnn_pop = p.Population(OUT_WIDTH * OUT_HEIGHT, celltype(**cell_params),
+                            structure=p.Grid2D(OUT_WIDTH / OUT_HEIGHT), label=CNN_POP_LABEL)
 
-    target_pop = p.Population(
-        out_width * out_height, celltype(**cell_params),
-        structure=p.Grid2D(out_width / out_height), label=POP_LABEL)
 
-    p.Projection(spif_retina, target_pop, convolution, p.Convolution())
+    # Setting XY Projection Layer
+    xyp_pop = p.Population(OUT_WIDTH + OUT_HEIGHT, celltype(**cell_params), label=XYP_POP_LABEL)
+    conn_list = create_conn_list(OUT_WIDTH, OUT_HEIGHT)
+    cell_conn = p.FromListConnector(conn_list, safe=True)     
 
-    spif_output = p.Population(None, p.external_devices.SPIFOutputDevice(
-        database_notify_port_num=conn.local_port, chip_coords=CHIP), label="output")
-    p.external_devices.activate_live_output_to(target_pop, spif_output)
+    
+    # Projections
+    p.Projection(p_spif_virtual, cnn_pop, convolution, p.Convolution())
+    p.Projection(cnn_pop, xyp_pop, cell_conn, receptor_type='excitatory')
+
+    # Setting up SPIF Output
+    if showing_cnn:
+        conn_cnn = p.external_devices.SPIFLiveSpikesConnection([CNN_POP_LABEL], SPIF_IP_A, SPIF_PORT)
+        conn_cnn.add_receive_callback(CNN_POP_LABEL, recv_nid)
+        spif_cnn_output = p.Population(None, p.external_devices.SPIFOutputDevice(
+            database_notify_port_num=conn_cnn.local_port, chip_coords=CHIP), label="cnn_output")
+        p.external_devices.activate_live_output_to(cnn_pop, spif_cnn_output)
+    else:
+        conn_xyp = p.external_devices.SPIFLiveSpikesConnection([XYP_POP_LABEL], SPIF_IP_A, SPIF_PORT)
+        conn_xyp.add_receive_callback(XYP_POP_LABEL, recv_nid_rc)
+        spif_xyp_output = p.Population(None, p.external_devices.SPIFOutputDevice(
+            database_notify_port_num=conn_xyp.local_port, chip_coords=CHIP), label="xyp_output")
+        p.external_devices.activate_live_output_to(xyp_pop, spif_xyp_output)
 
 
     try:
+        time.sleep(1)
         print("List of parameters:")
         print(f"\tNPC: {NPC_X} x {NPC_Y}")
-        print(f"\tOutput {out_width} x {out_height}")
+        print(f"\tOutput {OUT_WIDTH} x {OUT_HEIGHT}")
         print(f"\tKernel Size: {len(kernel)}")
         print(f"\tKernel Sum: {abs(round(np.sum(kernel),3))}")
         user_input = input_with_timeout("Happy?\n ", 10)
