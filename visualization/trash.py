e12c6779312ea8602c5e5a270e018eecccb17005
Juan Pablo Romero Bermudez <jprb@kth.se>
2023-10-04 20:25:49 +0200

    Sonification

--- visualization/trash.py
+++ visualization/trash.py
@@ -1,16 +1,9 @@
+import time
+from playsound import playsound
 
-import numpy as np
+while True:
 
-def split_and_sum(original_array, block_size):
-    n, m = original_array.shape
-    result_array = original_array.reshape(n // block_size, block_size, m // block_size, block_size).sum(axis=(1, 3))
-    return result_array
+    playsound('sound.mp3')
 
-
-import numpy as np
-
-# Create a random 4x4 integer NumPy array for demonstration
-original_array = np.random.randint(1, 5, (12, 7))  # Generates random integers between 1 and 4
-print(original_array)
-result_array = split_and_sum(original_array, block_size=4)
-print(result_array)
+    # Allow some time for the sound to finish playing
+    time.sleep(1)  # Adjust the sleep time as needed based on your sound file's duration
