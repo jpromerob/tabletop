5f7a8e9bd1ea3141788b879e65524e19f7183677
Juan Pablo Romero Bermudez <jprb@kth.se>
2024-04-19 12:17:27 +0200

    Updating auto_test with reasonable sparsity|delta combinations

--- analysis/auto_test.py
+++ analysis/auto_test.py
@@ -1,28 +1,47 @@
 import os
 from datetime import datetime
 import time
+import pdb
+
+# min_sparsity, max_sparsity, min_delta, max_delta
+speed_mapper = {
+    "low": (0.08, 0.12, 0.4, 1),
+    "medium": (0.14, 0.36, 1.8, 3),
+    "high": (0.4, 0.8, 4, 12)
+}
+
 
 nb_runs = 10
-max_sparsity = 0.8
-min_sparsity = 0.2
-max_delta = 5
-min_delta = 2
-
 nb_pts = 10000
 
-for i in range(nb_runs):
 
-    print(f"Run #{i+1}/{nb_runs}")
+for speed in speed_mapper:
+    print("\n\n\n*****************************************")
+    print(f"Starting simulation for {speed} speed")
+    print("*****************************************\n\n\n")
+    min_sparsity = speed_mapper[speed][0]
+    max_sparsity = speed_mapper[speed][1]
+    min_delta = speed_mapper[speed][2]
+    max_delta = speed_mapper[speed][3]
 
-    fname = f"Synthetic_{datetime.now().strftime('%y%m%d_%H%M%S')}"
+    for i in range(nb_runs):
 
-    sparsity = round(min_sparsity+i*(max_sparsity-min_sparsity)/nb_runs,3)
+        print(f"\n\n{speed} run #{i+1}/{nb_runs}")
 
-    delta = round(min_delta+i*(max_delta-min_delta)/nb_runs,3)
-    print(f"Fname: {fname} | Sparsity: {sparsity} | Delta: {delta}")
+        fname = f"Synthetic_{datetime.now().strftime('%y%m%d_%H%M%S')}"
 
-    os.system(f"python3 gen_analyzer.py -n {nb_pts} -s {sparsity} -d {delta} -f {fname} -g")
-    time.sleep(5)
+        sparsity = round(min_sparsity+i*(max_sparsity-min_sparsity)/nb_runs,3)
 
-    os.system(f"python3 gen_analyzer.py -n {nb_pts} -s {sparsity} -d {delta} -f {fname}")
-    time.sleep(5)
+        delta = round(min_delta+i*(max_delta-min_delta)/nb_runs,3)
+        print(f"Fname: {fname} | Sparsity: {sparsity} | Delta: {delta}")
+
+        os.system(f"python3 gen_analyzer.py -n {nb_pts} -s {sparsity} -d {delta} -f {fname} -g")
+        time.sleep(5)
+
+        os.system(f"python3 gen_analyzer.py -n {nb_pts} -s {sparsity} -d {delta} -f {fname}")
+        time.sleep(5)
+    
+
+
+
+
